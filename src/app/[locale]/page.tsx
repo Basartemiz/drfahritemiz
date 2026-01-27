6eefe61548ab330bf2e097d84e4babc40e31ee8b
basar_temiz <basar_temiz@basar-temizs-MacBook-Pro-2.local>
2026-01-27 22:43:10 +0300

    Remove Konak keyword and delete UpcomingTopics section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -3,7 +3,6 @@ import Features from '@/components/home/Features';
 import AboutPreview from '@/components/home/AboutPreview';
 import ServicesGrid from '@/components/home/ServicesGrid';
 import TestimonialsCarousel from '@/components/home/TestimonialsCarousel';
-import UpcomingTopics from '@/components/home/UpcomingTopics';
 import VideoSection from '@/components/home/VideoSection';
 import CTASection from '@/components/home/CTASection';
 
@@ -15,7 +14,6 @@ export default function HomePage() {
       <AboutPreview />
       <ServicesGrid />
       <TestimonialsCarousel />
-      <UpcomingTopics />
       <VideoSection />
       <CTASection />
     </>
